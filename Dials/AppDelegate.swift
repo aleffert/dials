9280d073c6e6b2f1d7fe1362e2172d3b8bb33ac5
Akiva Leffert <aleffert@gmail.com>
2014-12-05 22:21:46 -0500

    Initial Commit

---
+++ Dials/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dials
+//
+//  Created by Akiva Leffert on 12/5/14.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
